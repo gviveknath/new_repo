e699efefd88f91038a5e68c752401a35f0320fb1
gviveknath <113289632+gviveknath@users.noreply.github.com>
2022-10-19 21:42:18 +0530

    Update bulid.groovy

--- vars/bulid.groovy
+++ vars/bulid.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 def call(){
-   pipeline{
-    agent any
-    tools {
-  git 'Default'
-  maven 'maven'
-}
-     stage("bulid"){
-            steps{
+    pipeline{
+        agent any
+        tools{
+            git 'Default'
+            maven 'maven'
+        }
+        stage('bulid'){
+            step{
                 echo "========executing bulid========"
                 sh 'mvn clean install'
             }
-     }
-     }
-     }
+        }
+    }
+}
